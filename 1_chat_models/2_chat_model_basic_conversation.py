e70b59b75f2481e5e3b9609ac870f24212933084
nugh75 <daniele.dragoni@gmail.com>
2024-07-15 18:51:33 +0200

    15 luglio 19

--- 1_chat_models/2_chat_model_basic_conversation.py
+++ 1_chat_models/2_chat_model_basic_conversation.py
@@ -6,20 +6,21 @@ from langchain_openai import ChatOpenAI
 load_dotenv()
 
 # Create a ChatOpenAI model
-model = ChatOpenAI(model="gpt-4o")
+#model = ChatOpenAI(model="gpt-4o")
+model = ChatOpenAI(base_url="http://localhost:11434/v1", model="llama3")
 
 # SystemMessage:
 #   Message for priming AI behavior, usually passed in as the first of a sequenc of input messages.
 # HumanMessagse:
-#   Message from a human to the AI model.
-messages = [
-    SystemMessage(content="Solve the following math problems"),
-    HumanMessage(content="What is 81 divided by 9?"),
-]
+# #   Message from a human to the AI model.
+# messages = [
+#     SystemMessage(content="Solve the following math problems"),
+#     HumanMessage(content="What is 81 divided by 9?"),
+# ]
 
-# Invoke the model with messages
-result = model.invoke(messages)
-print(f"Answer from AI: {result.content}")
+# # Invoke the model with messages
+# result = model.invoke(messages)
+# print(f"Answer from AI: {result.content}")
 
 
 # AIMessage:
--- 4_rag/1a_rag_basics.py
+++ 4_rag/1a_rag_basics.py
@@ -1,9 +1,11 @@
 import os
-
+from dotenv import load_dotenv
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 
 # Define the directory containing the text file and the persistent directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -35,9 +37,9 @@ if not os.path.exists(persistent_directory):
 
     # Create embeddings
     print("\n--- Creating embeddings ---")
-    embeddings = OpenAIEmbeddings(
-        model="text-embedding-3-small"
-    )  # Update to a valid embedding model if needed
+    # model_name="sentence-transformers/all-MiniLM-L12-v2"
+    # embeddings= HuggingFaceEmbeddings(model_name=model_name)
+    embeddings = OpenAIEmbeddings(model="text-embedding-3-small")  # Update to a valid embedding model if needed
     print("\n--- Finished creating embeddings ---")
 
     # Create the vector store and persist it automatically
